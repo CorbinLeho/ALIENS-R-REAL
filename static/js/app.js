59fdb301417d56729db36a80c4e2a775dd271bea
Corbin Leho <corbinleho@yahoo.com>
2019-02-03 19:31:10 -0500

    search

--- static/js/app.js
+++ static/js/app.js
@@ -9,33 +9,33 @@ var tbody = d3.select("tbody");
 console.log(tableData);
 
 // // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// data.forEach(function(UFO_Sighting) {
+//   console.log(UFO_Sighting);
 // });
 
 // // Step 2:  Use d3 to append one table row `tr` for each weather report object
 // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// data.forEach(function(UFO_Sighting) {
+//   console.log(UFO_Sighting);
 //   var row = tbody.append("tr");
 // });
 
 // // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// data.forEach(function(UFO_Sighting) {
+//   console.log(UFO_Sighting);
 //   var row = tbody.append("tr");
 
-//   Object.entries(weatherReport).forEach(function([key, value]) {
+//   Object.entries(UFO_Sighting).forEach(function([key, value]) {
 //     console.log(key, value);
 //   });
 // });
 
 // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// data.forEach(function(UFO_Sighting) {
+//   console.log(UFO_Sighting);
 //   var row = tbody.append("tr");
 
-//   Object.entries(weatherReport).forEach(function([key, value]) {
+//   Object.entries(UFO_Sighting).forEach(function([key, value]) {
 //     console.log(key, value);
 //     // Append a cell to the row for each value
 //     // in the weather report object
@@ -45,10 +45,10 @@ console.log(tableData);
 
 // // Step 5: Use d3 to update each cell's text with
 // // weather report values (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
+// data.forEach(function(UFO_Sighting) {
+//   console.log(UFO_Sighting);
 //   var row = tbody.append("tr");
-//   Object.entries(weatherReport).forEach(function([key, value]) {
+//   Object.entries(UFO_Sighting).forEach(function([key, value]) {
 //     console.log(key, value);
 //     // Append a cell to the row for each value
 //     // in the weather report object
@@ -58,10 +58,35 @@ console.log(tableData);
 // });
 
 // BONUS: Refactor to use Arrow Functions!
-data.forEach((weatherReport) => {
+data.forEach((UFO_Sighting) => {
   var row = tbody.append("tr");
-  Object.entries(weatherReport).forEach(([key, value]) => {
+  Object.entries(UFO_Sighting).forEach(([key, value]) => {
     var cell = tbody.append("td");
     cell.text(value);
   });
+});
+
+
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#search");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  // console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue || tableData.shape === inputValue || tableData.country === inputValue || tableData.city === inputValue || tableData.state === inputValue);
+
+  console.log(filteredData);
+
 });
